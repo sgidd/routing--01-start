bf8676e426fb0e677e8ea6f7831959d5818b3390
sunil <sunilgidd051@gmail.com>
2020-05-21 23:00:15 +0530

    Creating Dynamic Nested Routes

--- src/containers/FullPost/FullPost.js
+++ src/containers/FullPost/FullPost.js
@@ -10,8 +10,17 @@ class FullPost extends Component {
 
     componentDidMount () {
         console.log(this.props);
+        this.loadData();
+    }
+
+    componentDidUpdate(){
+        this.loadData();
+    }
+
+    loadData() {
         if ( this.props.match.params.id ) {
-            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ) {
+            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id) ) {
+                //another way we can convert this check to where only checks its value  this.state.loadedPost.id != this.props.match.params.id)
                 axios.get( '/posts/' + this.props.match.params.id )
                     .then( response => {
                         // console.log(response);
